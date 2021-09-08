d48596bb02b55c60bac38a3f1c8edd518048cfb9
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2021-09-08 17:07:29 +0300

    Add syntax and pretty printer for minimal calculus

---
+++ src/Phi/Minimal/Pretty.elm
@@ -0,0 +1,61 @@
+module Phi.Minimal.Pretty exposing (..)
+
+import Phi.Minimal.Syntax exposing (..)
+import Dict
+import Tuple
+
+-- |
+--
+-- > ppTerm (Locator 3)
+-- "ρ.ρ.ρ" : String
+ppTerm : Term -> String
+ppTerm term =
+  case term of
+    Object o ->
+      if Dict.isEmpty o
+        then "⟦⟧"
+        else ppObject o
+    App t (a, s) ->
+      ppTerm t ++ parens (ppAttrWithValue (a, Attached s))
+    Dot t a -> ppTerm t ++ "." ++ ppAttr a
+    Locator n -> ppLocator n
+
+ppLocator : Int -> String
+ppLocator n =
+  case n of
+    0 -> "ξ"
+    1 -> "ρ"
+    _ -> "ρ." ++ ppLocator (n - 1)
+
+ppObject : Object -> String
+ppObject
+  = enclose "⟦ " " ⟧"
+ << punctuate ", "
+ << List.map ppAttrWithValue
+ << List.sortBy Tuple.first
+ << Dict.toList
+
+ppAttrWithValue : (Attr, AttrValue) -> String
+ppAttrWithValue (a, t) = ppAttr a ++ " ↦ " ++ ppAttrValue t
+
+ppAttrValue : AttrValue -> String
+ppAttrValue v =
+  case v of
+    Void -> "∅"
+    Attached t -> ppTerm t
+
+ppAttr : Attr -> String
+ppAttr a = a
+--  case a of
+--    AttrIdent x -> x
+--    AttrPhi     -> "𝜑"
+--    AttrDelta   -> "δ"
+
+parens : String -> String
+parens = enclose "(" ")"
+
+enclose : String -> String -> String -> String
+enclose l r s = l ++ s ++ r
+
+punctuate : String -> List String -> String
+punctuate sep = String.concat << List.intersperse sep
---
+++ src/Phi/Minimal/Syntax.elm
@@ -0,0 +1,96 @@
+module Phi.Minimal.Syntax exposing (..)
+
+{-| This module defines the syntax for the minimal phi calculus.
+
+# Definition
+@docs Attr
+
+# Common Helpers
+@docs map, withDefault, oneOf
+
+# Chaining Maybes
+@docs andThen
+
+-}
+
+import Dict exposing (Dict)
+
+type alias Attr = String
+
+type AttrValue
+  = Void
+  | Attached Term
+
+type alias Object = Dict Attr AttrValue
+
+type alias Substitution = Dict Int Term
+
+type Term
+  = Object Object
+  | Dot Term Attr
+  | App Term (Attr, Term)
+  | Locator Int
+
+substituteXi : Term -> Term -> Term
+substituteXi = substituteLocator 0
+
+substituteLocator : Int -> Term -> Term -> Term
+substituteLocator k parent term =
+  case term of
+    Locator n ->
+      if n == k
+        then parent
+        else Locator n
+    Dot t a ->
+      Dot (substituteLocator k parent t) a
+    App t (a, u) ->
+      App (substituteLocator k parent t) (a, substituteLocator k parent u)
+    Object o -> Object (Dict.map (\a t ->
+      case t of
+        Void -> Void
+        Attached u -> Attached (substituteLocator (k + 1) (incLocators parent) u)
+      ) o)
+
+incLocators : Term -> Term
+incLocators = incLocatorsFrom 0
+
+incLocatorsFrom : Int -> Term -> Term
+incLocatorsFrom k term =
+  case term of
+    Object o -> Object (Dict.map (\a t ->
+      case t of
+        Void -> Void
+        Attached u -> Attached (incLocatorsFrom (k + 1) u)) o)
+    Dot t a -> Dot (incLocatorsFrom k t) a
+    App t (a, u) -> App (incLocatorsFrom k t) (a, incLocatorsFrom k u)
+    Locator n ->
+      if n >= k
+        then Locator (n + 1)
+        else Locator n
+
+whnf : Term -> Term
+whnf term =
+  case term of
+    Locator n -> Locator n
+    App t (a, u) ->
+      case whnf t of
+        Object o ->
+          case Dict.get a o of
+            Just Void -> Object (Dict.insert a (Attached (incLocators u)) o)
+            Just _  -> App (Object o) (a, u)
+            Nothing -> App (Object o) (a, u)
+        t2 -> App t2 (a, u)
+
+    Dot t a ->
+      case whnf t of
+        Object o ->
+          case Dict.get a o of
+            Just (Attached u) -> substituteXi u (Object o)
+            Just Void -> Dot (Object o) a
+            Nothing ->
+              if Dict.member "𝜑" o
+                then Dot (Dot (Object o) "𝜑") a
+                else Dot (Object o) a
+        t2 -> Dot t2 a
+
+    Object o -> Object o

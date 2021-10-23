90b4365d522d3f5becdd22b5ad14e3b2a1f7fc65
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2021-10-23 17:39:55 +0300

    add ex2, ex3

--- src/Minimal/Examples.elm
+++ src/Minimal/Examples.elm
@@ -12,6 +12,55 @@ getTerm s =
         _ -> Locator 0
 
 -- lines 6-10
+
+-- [
+--     book3 ->[
+--         isbn-> ?,
+--         title->^.ObjectThinking,
+--         price->^.memory,
+--         setprice->[
+--             p->?,
+--             @->^.price.write(t->$.p)
+--         ]
+--     ]
+-- ]
 ex1 : Term
-ex1 = getTerm "[book3 ->[\n isbn-> ?,\n title->^.ObjectThinking,\n price->^.memory,\n setprice->[p->?,@->^.price.write(t->$.p)]\n ]]"
-    
+ex1 = getTerm "[\n    book3 ->[\n        isbn-> ?,\n        title->^.ObjectThinking,\n        price->^.memory,\n        setprice->[\n            p->?,\n            @->^.price.write(t->$.p)\n        ]\n    ]\n]"
+
+
+-- lines 67-73
+
+-- [
+--     point -> [
+--         x -> ?,
+--         y -> ?,
+--         distance -> [
+--             to -> ?,
+--             len -> ^.^.^.vector
+--                 (arg1 -> $.to.x.sub(arg->^.x))
+--                 (arg2 -> $.to.x.sub(arg->^.y))
+--                 .length
+--         ]
+--     ]
+-- ]
+ex2 : Term
+ex2 = getTerm "[\n    point -> [\n        x -> ?,\n        y -> ?,\n        distance -> [\n            to -> ?,\n            len -> ^.^.^.vector\n                (arg1 -> $.to.x.sub(arg->^.x))\n                (arg2 -> $.to.x.sub(arg->^.y))\n                .length\n        ]\n    ]\n]"
+
+
+-- example 14
+
+-- [
+--     c -> [
+--         center -> ^.point
+--             (arg1 -> ^.negative3)
+--             (arg1 -> ^.positive9),
+--         radius -> ^.positive40,
+--         @ -> $.center,
+--         is_inside -> [
+--             p -> ?,
+--             @ -> ^.distance(arg -> $.p).leq(arg -> $.p.radius)
+--         ]
+--     ]
+-- ]
+ex3 : Term
+ex3 = getTerm "[\n    c -> [\n        center -> ^.point\n            (arg1 -> ^.negative3)\n            (arg1 -> ^.positive9),\n        radius -> ^.positive40,\n        @ -> $.center,\n        is_inside -> [\n            p -> ?,\n            @ -> ^.distance(arg -> $.p).leq(arg -> p.radius)\n        ]\n    ]\n]"

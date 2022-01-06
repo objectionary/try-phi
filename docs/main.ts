07ba897ac7a814a5e593c7c5c18e4c9aa5dd6d3e
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-01-06 21:48:56 +0300

    add inline anonymous

--- docs/main.ts
+++ docs/main.ts
@@ -16,7 +16,7 @@ main > [args...]
     @ >
       or.
         and.
-          eq. (mod. y 4.0e-1) 0A-BB
+          eq. ([x] (y > 4.0e-1)) 0A-BB
           not. (eq. (mod. y 0x13a) --)
         eq. (mod. y 400.) TRUE
   @ >

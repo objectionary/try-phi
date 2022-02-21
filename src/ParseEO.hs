f41881b6258e1301bde884fc4fdd1b6bead3fbac
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-02-21 20:42:50 +0300

    try bnf converter

--- src/ParseEO.hs
+++ src/ParseEO.hs
@@ -1,43 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-module ParseEO ( pUri ) where
-import Control.Applicative
-import Control.Monad
-import Data.Text (Text)
-import Data.Void
-import Text.Megaparsec hiding (State)
-import Text.Megaparsec.Char
-import qualified Data.Text as T
-import qualified Text.Megaparsec.Char.Lexer as L
+module ParseEO where
 
-type Parser = Parsec Void Text
+import Data.Char ( ord, isDigit )
 
-pUri :: Parser Uri
-pUri = do
-  r <- pScheme
-  _ <- char ':'
-  return (Uri r)
+data Board = B [Int]
 
-data Scheme
-  = SchemeData
-  | SchemeFile
-  | SchemeFtp
-  | SchemeHttp
-  | SchemeHttps
-  | SchemeIrc
-  | SchemeMailto
-  deriving (Eq, Show)
+nextPositions :: Board -> [Board]
+nextPositions b = [b]
 
-pScheme :: Parser Scheme
-pScheme = choice
-  [ SchemeData   <$ string "data"
-  , SchemeFile   <$ string "file"
-  , SchemeFtp    <$ string "ftp"
-  , SchemeHttp   <$ string "http"
-  , SchemeHttps  <$ string "https"
-  , SchemeIrc    <$ string "irc"
-  , SchemeMailto <$ string "mailto" ]
+f n 
+    | n < 0 = []
+    | otherwise = 
 
-data Uri = Uri
-  { uriScheme :: Scheme
-  } deriving (Eq, Show)
+nextPositionsN :: Board -> Int -> (Board -> Bool) -> [Board]
+nextPositionsN b n pred = do
+    

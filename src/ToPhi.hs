0d3b6ae0bc16c64fa1781d236bed26d3f5b20675
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-07 12:02:58 +0300

    upd

--- src/ToPhi.hs
+++ src/ToPhi.hs
@@ -1,37 +1,56 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# HLINT ignore "Use newtype instead of data" #-}
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE OverloadedLists            #-}
+{-# LANGUAGE PatternSynonyms            #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE ViewPatterns               #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module ToPhi where
 
 
-import ToTerm ()
+import           Data.HashMap.Strict.InsOrd (InsOrdHashMap)
+import qualified Data.HashMap.Strict.InsOrd as InsOrdHashMap
+import GHC.Exts (IsList (..))
+
+type Attr = String
+
+newtype Object a = Object
+  { getObject :: InsOrdHashMap Attr (AttrValue a)
+  }
+  deriving (Eq, Functor, Foldable, Traversable, IsList)
+
+(.?) :: Object a -> Attr -> Maybe (AttrValue a)
+Object o .? a = InsOrdHashMap.lookup a o
+
+(.=) :: Object a -> (Attr, a) -> Object a
+Object o .= (a, v) = Object (InsOrdHashMap.insert a (Attached v) o)
+
+data AttrValue a
+  = VoidAttr
+  | Attached a
+  deriving (Eq, Functor, Foldable, Traversable)
+
+splitAttrs :: Object a -> ([Attr], [(Attr, a)])
+splitAttrs = foldr add ([], []) . toList
+  where
+    add (a, VoidAttr) (void, attached)   = (a : void, attached)
+    add (a, Attached t) (void, attached) = (void, (a, t) : attached)
 
 
-{-
-What can we reuse?
-
--}
-
-type AttrName = String
-
--- newtype Object a = Object
---   { getObject :: InsOrdHashMap Attr (AttrValue a)
---   }
---   deriving (Eq, Functor, Foldable, Traversable, IsList)
+data DataValue =
+  DataInteger Integer
+  | NoData
+  deriving (Eq, Show, Ord)
 
 
--- data Term
---   = Obj (Object Term)
---   | Dot Term Attr
---   | App Term (Attr, Term)
---   | Loc Int
---   | DataTerm DataValue
--- data Term
---   = App {name :: Term, }
+data Term
+  = Obj (Object Term)
+  | Dot Term Attr
+  | App Term (Attr, Term)
+  | Loc Int
+  | DataTerm DataValue
+  deriving (Eq)

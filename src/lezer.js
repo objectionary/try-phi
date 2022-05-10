11356fc71c8f817347a1013cd030f2f947baab58
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-10 08:16:46 +0300

    move scripts

--- src/lezer.js
+++ src/lezer.js
@@ -1,4 +1,4 @@
-import {parser} from '../src/parser'
+import {parser} from './parser'
 import {foldNodeProp, LezerLanguage} from '@codemirror/next/language'
 import {styleTags, tags as t} from '@codemirror/next/highlight'
 
@@ -7,9 +7,7 @@ const foldInner = tree => ({ from: tree.start + 1, to: tree.end - 1 })
 export const lezerLanguage = LezerLanguage.define({
   parser: parser.configure({
     props: [
-      foldNodeProp.add({
-
-      }),
+      foldNodeProp.add({}),
       styleTags({
         Locator: t.keyword,
         Attr: t.integer,

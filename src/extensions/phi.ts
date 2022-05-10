624cca6407588e237aa987035362c8f415ade3d5
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-10 15:20:39 +0300

    allow decorator as an attribute, fix regex, change colors

--- src/extensions/phi.ts
+++ src/extensions/phi.ts
@@ -12,14 +12,31 @@ export const phiLanguage = LRLanguage.define({
     props: [
       styleTags({
         Locator: t.keyword,
-        Attr: t.attributeName,
-        Mapsto: t.operatorKeyword,
+        VOID: t.keyword,
+
+        Name: t.attributeName,
         Decorator: t.labelName,
+                
+        Mapsto: t.operatorKeyword,
+        DOT: t.operatorKeyword,
+
+        BOOL: t.integer,
+        INT: t.integer,
+        BYTES: t.integer,
+        HEX: t.integer,
+        FLOAT: t.integer,
+        
+        REGEX: t.regexp,
+
+        STRING: t.string,
+        TEXT: t.string,
+        CHAR: t.string,
+        
         LP: t.paren,
         RP: t.paren,
+
         LSQ: t.squareBracket,
         RSQ: t.squareBracket,
-        DOT: t.operatorKeyword
       }),
     ],
   }),
@@ -29,20 +46,20 @@ export const phiLanguage = LRLanguage.define({
 })
 
 export const phiHighlighting = HighlightStyle.define([
-  { tag: tags.keyword, color: '#195791' },
+  { tag: tags.keyword, color: '#7826e2' },
+  { tag: tags.attributeName, color: '#000000' },
   { tag: tags.operatorKeyword, color: '#A626A4' },
   { tag: tags.labelName, color: '#986801' },
-  { tag: tags.attributeName, color: '#000000' },
   { tag: tags.paren, color: '#383A42' },
   { tag: tags.squareBracket, color: '#383A42' },
-  { tag: tags.integer, color: '#7826e2' },
 
+  { tag: tags.integer, color: '#986801' },
   { tag: tags.bool, color: '#986801' },
   { tag: tags.number, color: '#986801' },
   { tag: tags.float, color: '#986801' },
   { tag: tags.regexp, color: '#0184BC' },
-  { tag: tags.string, color: '#50A14F' },
-  { tag: tags.character, color: '#0184BC' },
+  { tag: tags.string, color: '#598559' },
+  { tag: tags.character, color: '#598559' },
 ])
 
 export function phi() {

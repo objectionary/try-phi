7f14fcde5d870e79828d609f608440b3f723275b
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-10 20:43:56 +0300

    fix scripts

--- Site/scripts/get-code.js
+++ Site/scripts/get-code.js
@@ -2,6 +2,35 @@ function pollDOM() {
   if (typeof app !== 'undefined') {
     return app.view.state.doc.toString()
   }
+  else {
+    let p = 
+`[
+  price -> 2,
+  book -> [
+    title -> ^2.title,
+    price -> ?
+  ],
+  manga -> [
+    manga_title -> ^2.str_publisher.add(str -> ^0.title),
+    @ -> ^1.book
+  ](price -> ^0.price),
+  title -> ^0.manga.manga_title
+].manga.price`;
+    return p
+  }
 }
 
 pollDOM()
+
+// var _TIMEOUT = 50; // waitfor test rate [msec]
+// waitfor(() => (typeof app !== 'undefined'), true, _TIMEOUT, 0, 'play->busy false', function() {
+//   return app.view.state.doc.toString()
+// })
+
+// (async() => {
+//   console.log("waiting for variable");
+//   while(!window.hasOwnProperty("app")) // define the condition as you like
+//       await new Promise(resolve => setTimeout(resolve, 100));
+//   return app.view.state.doc.toString()
+// })();
+// console.log("above code doesn't block main function stack");

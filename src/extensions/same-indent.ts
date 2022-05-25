8d3d06831aa748d3eac73f95de15bc26c0952a5f
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-05-25 18:32:40 +0300

    fix indents

--- src/extensions/same-indent.ts
+++ src/extensions/same-indent.ts
@@ -2,8 +2,14 @@ import { indentService, IndentContext } from '@codemirror/language'
 
 function getPreviousIndent(context: IndentContext, pos: number) {
   let doc = context.state.doc
-  let lineBefore = doc.lineAt(pos).number
-  let currentIndent = context.lineIndent(doc.line(lineBefore).from)
+  let currentLine = doc.line(doc.lineAt(pos).number)
+  let currentIndent = context.lineIndent(currentLine.from)
+
+  let cursorRange = context.state.selection.main
+  let cursorPos = cursorRange.from
+  if (cursorPos != currentLine.to && cursorPos != currentIndent + currentLine.from){
+    return currentIndent + 2
+  }
   return currentIndent
 }
 

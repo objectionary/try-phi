0731d8823020318cdaffa6cb9103311d9b517ca4
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-14 13:15:44 +0300

    add tools to back flake

--- back/flake.nix
+++ back/flake.nix
@@ -1,11 +1,17 @@
 {
   description = "Try-phi back end";
   inputs = {
+    # common-flake.url = "github:objectionary/try-phi?dir=common-flake";
     nixpkgs.url = "github:NixOS/nixpkgs/cd8bbdd4fdb15f7758fd3c8df531e9d42bee239d";
     flake-utils.url = "github:numtide/flake-utils";
     gitignore = {
       url = "github:hercules-ci/gitignore.nix/a20de23b925fd8264fd7fad6454652e142fd7f73";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    my-codium = {
+      url = "github:br4ch1st0chr0n3/flakes?dir=codium&rev=644841e48b858417353b85c97dfc06e234025145";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
@@ -14,30 +20,34 @@
     , nixpkgs
     , flake-utils
     , gitignore
+    , my-codium
+    # , common-flake
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-
-      # need to match Stackage LTS version from stack.yaml resolver
-      hPkgs = pkgs.haskell.packages."ghc902";
-
+      ghcVersion = "902";
+      inherit (my-codium.tools.${system})
+        allShellTools
+        toolsGHC
+        justStaticExecutables
+        ;
+      
+      inherit (toolsGHC ghcVersion) stack callCabal;
+      
       try-phi-back =
         let
-          inherit (hPkgs) callCabal2nix;
-          inherit (pkgs.haskell.lib) justStaticExecutables;
-          inherit (gitignore.lib) gitignoreSource;
-
-          eo-utils = callCabal2nix "eo-utils" (gitignoreSource ./language-utils/eo-utils) { };
-          phi-utils = callCabal2nix "phi-utils" (gitignoreSource ./language-utils/phi-utils) { };
-          language-utils = callCabal2nix "language-utils" (gitignoreSource ./language-utils) {
+          eo-utils = callCabal "eo-utils" ./language-utils/eo-utils { };
+          phi-utils = callCabal "phi-utils" ./language-utils/phi-utils { };
+          language-utils = callCabal "language-utils" ./language-utils {
             inherit phi-utils eo-utils;
           };
-          back = callCabal2nix "try-phi-back" (gitignoreSource ./.) {
+          back = callCabal "try-phi-back" ./. {
             inherit language-utils phi-utils eo-utils;
           };
         in
         justStaticExecutables back;
+
       back = pkgs.stdenv.mkDerivation {
         buildInputs = [ try-phi-back ];
         name = "back";
@@ -47,6 +57,14 @@
           ln -s ${try-phi-back}/bin/try-phi-back-exe $out/bin/back
         ";
       };
+
+      tools = builtins.attrValues {
+        inherit (allShellTools)
+          implicit-hie
+          ghcid
+          ;
+        inherit stack;
+      };
     in
     {
       packages = {
@@ -55,7 +73,7 @@
 
       devShells = {
         default = pkgs.mkShell {
-          buildInputs = [ back ];
+          buildInputs = tools;
 
           # https://stackoverflow.com/a/63751678
           shellHook = ''
